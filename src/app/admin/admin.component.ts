ed16eda9366de9e5f916f8e3ff966f6a9beca450
druidsmickey <61027381+druidsmickey@users.noreply.github.com>
2020-03-27 05:15:58 +0400

    Update admin.component.ts

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,17 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-	private menus = [];
+	public menus = [];
 	private selectedItem = {};
   constructor() { }
 
   ngOnInit() {
   	this.menus = [
-		{item:"User Accounts", link:"UserAccountsComponent"},
-		{item:"MenuTabs", link:"MenuTabsComponent"},
-		{item:"Brands", link:"BrandsComponent"},
-		{item:"Items", link:"ItemsComponent"},
-        {item:"Pricing", link:"PricingComponent"}
+  		{item:"User Accounts", link:"UserAccountsComponent"},
+  		{item:"MenuTabs", link:"MenuTabsComponent"},
+  		{item:"Brands", link:"BrandsComponent"},
+  		{item:"Items", link:"ItemsComponent"},
+      {item:"Pricing", link:"PricingComponent"}
     ];
   }
 
