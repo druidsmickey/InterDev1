34eeceed2ae3cba0a01d5bfc4d30c929a38cd2c1
aarudenko99 <aarudenko99@gmail.com>
2020-04-14 18:08:01 +0300

    update api path

--- frontend/src/app/common.service.ts
+++ frontend/src/app/common.service.ts
@@ -12,21 +12,21 @@ export class CommonService {
   private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
   private options = new RequestOptions({ headers: this.headers });
 
-  uri = 'http://localhost:8080';
+  uri = 'http://localhost:8080/api';
 
  	constructor(private http: HttpClient) {}
 
   getBrands() {
-    return this.http.get(`${this.uri}/brand`);
+    return this.http.get(`${this.uri}/data/brand`);
   }
 
   getCCTVs() {
-    return this.http.get(`${this.uri}/cctv`);
+    return this.http.get(`${this.uri}/data/cctv`);
   }
 
   addCCTVs(cctvs) {
     console.log('common.service', cctvs);
-    return this.http.post(`${this.uri}/addcctvs`, cctvs);
+    return this.http.post(`${this.uri}/data/addcctvs`, cctvs);
     // return this.http.post(`${this.uri}/addcctvs`, JSON.stringify(cctvs), this.options);
   }
 }
